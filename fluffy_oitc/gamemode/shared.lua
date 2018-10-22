3dc9ab5117cd12c646a9e3eeb59960733523c5db
FluffyXVI <fluffyxvi@gmail.com>
2018-10-22 19:47:26 +1000

    Simplified & commented

--- fluffy_oitc/gamemode/shared.lua
+++ fluffy_oitc/gamemode/shared.lua
@@ -3,13 +3,7 @@ DeriveGamemode('fluffy_mg_base')
 GM.Name = 'OITC'
 GM.Author = 'FluffyXVI'
 
-GM.TeamBased = true	-- Is the gamemode FFA or Teams?
-GM.RoundTime = 90
-GM.RoundNumber = 10
-GM.Elimination = true
-
-GM.DeathSounds = true
-
-function GM:Initialize()
-
-end
+GM.TeamBased = true		-- Is the gamemode FFA or Teams?
+GM.Elimination = true	-- Is this gamemode elimination?
+GM.RoundTime = 90 		-- How long each round should last, in seconds
+GM.RoundNumber = 10		-- How many rounds are in each game?
