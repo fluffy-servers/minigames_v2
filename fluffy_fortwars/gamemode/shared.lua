439eb76a0c3f23e6809b0a3ba032b8bc00fa0b22
FluffyXVI <fluffyxvi@gmail.com>
2018-11-12 16:56:34 +1000

    Fortwars initial commit

---
+++ fluffy_fortwars/gamemode/shared.lua
@@ -0,0 +1,12 @@
+DeriveGamemode('fluffy_mg_base')
+
+GM.Name = 'Fortwars'
+GM.Author = 'FluffyXVI'
+
+GM.TeamBased = true	-- Is the gamemode FFA or Teams?
+GM.RoundTime = 150
+GM.RoundNumber = 5
+
+function GM:Initialize()
+
+end
