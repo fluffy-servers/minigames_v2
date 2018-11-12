dcb9dffd3bce3a030fe5b37ed8a978dca0f8d6ce
FluffyXVI <fluffyxvi@gmail.com>
2018-11-12 21:18:04 +1000

    Improve commenting

--- fluffy_fortwars/gamemode/init.lua
+++ fluffy_fortwars/gamemode/init.lua
@@ -6,17 +6,22 @@ include('sv_props.lua')
 
 -- Handle everything to do with starting the build phase
 function GM:StartBuildPhase()
-
+    timer.Simple(100, function() GAMEMODE:EndBuildPhase() end)
 end
 
--- Finish up the build phase & start the
+-- Finish up the build phase & start the fighting phase
 function GM:EndBuildPhase()
-
+    -- Freeze all the props
+    -- Reset the loadouts
+    -- Remove any barriers & spawn the flags
 end
 
 function GM:PlayerLoadout(ply)
+    ply:StripWeapons()
     if GAMEMODE.ROUND_PHASE == "BUILDING" then
-        ply:StripWeapons()
         ply:Give("weapon_physgun")
+    else
+        -- weapons here
+        ply:Give("weapon_pistol")
     end
 end
