7404a420d7ecdb64350b6e1a55cdca6910b2c1b7
FluffyXVI <fluffyxvi@gmail.com>
2018-11-14 01:26:01 +1000

    Castle Wars file structure

---
+++ fluffy_castlecrash/gamemode/shared.lua
@@ -0,0 +1,12 @@
+DeriveGamemode('fluffy_mg_base')
+
+GM.Name = 'Castle Wars'
+GM.Author = 'FluffyXVI'
+
+GM.TeamBased = true	-- Is the gamemode FFA or Teams?
+GM.RoundTime = 200
+GM.RoundNumber = 3
+
+function GM:Initialize()
+
+end
