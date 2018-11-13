4ec40c26ad18e16a6214291ae248de451d5048b9
FluffyXVI <fluffyxvi@gmail.com>
2018-11-14 01:56:50 +1000

    King clientside displays

--- fluffy_kingmaker/gamemode/cl_init.lua
+++ fluffy_kingmaker/gamemode/cl_init.lua
@@ -5,3 +5,24 @@ GM.ScoringPaneEnabled = true
 function GM:ScoringPaneScore(ply)
 	return ply:GetNWInt("KingFrags", 0)
 end
+
+-- Draw BIG label above the King
+-- Being the best comes at a price
+function GM:PostPlayerDraw(ply)
+    if not ply:GetNWBool("IsKing", false) then return end
+    
+    local p = ply:GetPos() + Vector(0, 0, 64)
+    p = p:ToScreen()
+    
+    draw.SimpleText("King!", "FS_32", x+1, y+2, GAMEMODE.FColShadow, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    draw.SimpleText("King!", "FS_32", x, y, GAMEMODE.FCol1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+end
+
+hook.Add('HUDPaint', 'DrawKingNotice', function()
+    if not LocalPlayer():GetNWBool("IsKing", false) then return end
+    
+    local x = ScrW()/2
+    local y = ScrH() - 112
+    draw.SimpleText("You are the King!", "FS_32", x+1, y+2, GAMEMODE.FColShadow, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    draw.SimpleText("You are the King!", "FS_32", x, y, GAMEMODE.FCol1, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+end)
