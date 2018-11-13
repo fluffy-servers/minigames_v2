2683dd91e52d0023034409e2c0074a9f7291ec6a
FluffyXVI <fluffyxvi@gmail.com>
2018-11-14 02:35:51 +1000

    Last survivor bonus in Hunter gamemodes

--- fluffy_mg_base/gamemode/gametype_hunter.lua
+++ fluffy_mg_base/gamemode/gametype_hunter.lua
@@ -26,3 +26,36 @@ hook.Add('PlayerDeath', 'SurvivalDeath', function(ply)
         ply:SetTeam(GAMEMODE.HunterTeam)
     end
 end)
+
+-- Return false if multiple players are alive
+-- Return a single player if they are the last player alive
+function GM:GetLastPlayer(exclude_player)
+    local last_alive = nil
+    for k,v in pairs( player.GetAll() ) do
+        if v:Alive() and v:Team() == GAMEMODE.SurvivorTeam and !v.Spectating then
+            if exclude_player and v == exclude_player then continue end
+            print(v, exclude_player, 'alive!')
+            if IsValid(last_alive) then
+                return false
+            else
+                last_alive = v
+            end
+        end
+    end
+    return last_alive
+end
+
+-- Last Survivor gets a message and a stat bonus
+hook.Add('DoPlayerDeath', 'AwardLastSurvivor', function(ply)
+    if not GAMEMODE.TeamSurvival then return end
+    if GAMEMODE.DisableLoneSurvivor then return end
+    
+    if ply:Team() != GAMEMODE.SurvivorTeam then return end
+    local last_player = GAMEMODE:GetLastPlayer(ply)
+    if IsValid(last_player) and last_player != false then
+        -- Award the last survivor bonus
+        local name = string.sub(last_player:Nick(), 1, 10)
+        GAMEMODE:PulseAnnouncement(4, name .. ' is the lone survivor!', 0.8)
+        last_player:AddStatPoints('LastSurvivor', 1)
+    end
+end)
