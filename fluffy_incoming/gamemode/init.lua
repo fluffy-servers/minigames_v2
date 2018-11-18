ae7388da2547d05028435525cde082255e278922
FluffyXVI <fluffyxvi@gmail.com>
2018-11-18 16:08:10 +1000

    Update distance algorithm
    Turns out getting the position of a brush entity is unreliable at best and disastrous at worst. The better solution is, sadly, hardcoded values. These are implemented for all three of the default maps. This method will now give scoring that isn't completely wrong.

--- fluffy_incoming/gamemode/init.lua
+++ fluffy_incoming/gamemode/init.lua
@@ -2,129 +2,20 @@ AddCSLuaFile('cl_init.lua')
 AddCSLuaFile('shared.lua')
 
 include('shared.lua')
+include('sv_maps.lua')
 
 -- Nobody wins in Incoming ?
 function GM:GetWinningPlayer()
     return nil
 end
 
-INCMaps = {}
--- Default model list
-DefaultProps = {}
-DefaultProps['Geometric'] = {
-	"models/clannv/incoming/box/box1.mdl",
-	"models/clannv/incoming/box/box2.mdl",
-	"models/clannv/incoming/box/box3.mdl",
-	
-	"models/clannv/incoming/cone/cone1.mdl",
-	"models/clannv/incoming/cone/cone2.mdl",
-	"models/clannv/incoming/cone/cone3.mdl",
-	
-	"models/clannv/incoming/cylinder/cylinder1.mdl",
-	"models/clannv/incoming/cylinder/cylinder2.mdl",
-	"models/clannv/incoming/cylinder/cylinder3.mdl",
-	
-	"models/clannv/incoming/hexagon/hexagon1.mdl",
-	"models/clannv/incoming/hexagon/hexagon2.mdl",
-	"models/clannv/incoming/hexagon/hexagon3.mdl",
-	
-	"models/clannv/incoming/pentagon/pentagon1.mdl",
-	"models/clannv/incoming/pentagon/pentagon2.mdl",
-	"models/clannv/incoming/pentagon/pentagon3.mdl",
-	
-	"models/clannv/incoming/sphere/sphere1.mdl",
-	"models/clannv/incoming/sphere/sphere2.mdl",
-	"models/clannv/incoming/sphere/sphere3.mdl",
-	
-	"models/clannv/incoming/triangle/triangle1.mdl",
-	"models/clannv/incoming/triangle/triangle2.mdl",
-	"models/clannv/incoming/triangle/triangle3.mdl"
-}
-
-DefaultProps['Vehicles'] = {
-    'models/props_vehicles/van001a_physics.mdl',
-    'models/props_vehicles/car001a_hatchback.mdl',
-    'models/props_vehicles/car001b_hatchback.mdl',
-    'models/props_vehicles/car002a_physics.mdl',
-    'models/props_vehicles/car002b_physics.mdl',
-    'models/props_vehicles/car003a_physics.mdl',
-    'models/props_vehicles/car003b_physics.mdl',
-    'models/props_vehicles/car004a_physics.mdl',
-    'models/props_vehicles/car004b_physics.mdl',
-    'models/props_vehicles/car005a_physics.mdl',
-    'models/props_vehicles/car005b_physics.mdl',
-    'models/props_vehicles/apc001.mdl',
-    'models/props_vehicles/trailer001a.mdl',
-    'models/props_vehicles/trailer002a.mdl',
-    'models/props_vehicles/truck001a.mdl',
-    'models/props_vehicles/truck003a.mdl',
-}
-
-DefaultProps['Both'] = {
-    'models/props_vehicles/van001a_physics.mdl',
-    'models/props_vehicles/car001a_hatchback.mdl',
-    'models/props_vehicles/car001b_hatchback.mdl',
-    'models/props_vehicles/car002a_physics.mdl',
-    'models/props_vehicles/car002b_physics.mdl',
-    'models/props_vehicles/car003a_physics.mdl',
-    'models/props_vehicles/car003b_physics.mdl',
-    'models/props_vehicles/car004a_physics.mdl',
-    'models/props_vehicles/car004b_physics.mdl',
-    'models/props_vehicles/car005a_physics.mdl',
-    'models/props_vehicles/car005b_physics.mdl',
-    'models/props_vehicles/apc001.mdl',
-    'models/props_vehicles/trailer001a.mdl',
-    'models/props_vehicles/trailer002a.mdl',
-    'models/props_vehicles/truck001a.mdl',
-    'models/props_vehicles/truck003a.mdl',
-    
-    "models/clannv/incoming/box/box1.mdl",
-	"models/clannv/incoming/box/box2.mdl",
-	"models/clannv/incoming/box/box3.mdl",
-	
-	"models/clannv/incoming/cone/cone1.mdl",
-	"models/clannv/incoming/cone/cone2.mdl",
-	"models/clannv/incoming/cone/cone3.mdl",
-	
-	"models/clannv/incoming/cylinder/cylinder1.mdl",
-	"models/clannv/incoming/cylinder/cylinder2.mdl",
-	"models/clannv/incoming/cylinder/cylinder3.mdl",
-	
-	"models/clannv/incoming/hexagon/hexagon1.mdl",
-	"models/clannv/incoming/hexagon/hexagon2.mdl",
-	"models/clannv/incoming/hexagon/hexagon3.mdl",
-	
-	"models/clannv/incoming/pentagon/pentagon1.mdl",
-	"models/clannv/incoming/pentagon/pentagon2.mdl",
-	"models/clannv/incoming/pentagon/pentagon3.mdl",
-	
-	"models/clannv/incoming/sphere/sphere1.mdl",
-	"models/clannv/incoming/sphere/sphere2.mdl",
-	"models/clannv/incoming/sphere/sphere3.mdl",
-	
-	"models/clannv/incoming/triangle/triangle1.mdl",
-	"models/clannv/incoming/triangle/triangle2.mdl",
-	"models/clannv/incoming/triangle/triangle3.mdl"
-}
-
 -- No weapons
 function GM:PlayerLoadout( ply )
 
 end
 
-function GM:GetWinningEntity()
-    if GAMEMODE.WinningZone and IsValid(GAMEMODE.WinningZone) then
-        return GAMEMODE.WinningZone
-    else
-        local e = ents.FindByClass('inc_winners_area')[1]
-        if not IsValid(e) then
-            print('Map has no winning area')
-            return nil
-        end
-        
-        GAMEMODE.WinningZone = e
-        return GAMEMODE.WinningZone
-    end
+function GM:EndingPoint()
+    return GAMEMODE.MapInfo[game.GetMap()].endpos
 end
 
 CurrentPropsCategory = 'Both'
@@ -134,9 +25,9 @@ INCPropSpawnTimer = 0
 local Delay = 2
 hook.Add("Tick", "TickPropSpawn", function()
     if GetGlobalString('RoundState') != 'InRound' then return end
-    local Props = DefaultProps[ CurrentPropsCategory ]
+    local Props = GAMEMODE.DefaultProps[ CurrentPropsCategory ]
 	if ( INCPropSpawnTimer < CurTime() ) then
-		for k, v in pairs( ents.FindByClass( INCMaps["SpawnEntity"] or 'inc_prop_spawner' ) ) do
+		for k, v in pairs( ents.FindByClass('inc_prop_spawner') ) do
 			INCPropSpawnTimer = CurTime() + Delay
 			local Ent = ents.Create( "prop_physics" )
 			Ent:SetModel( Props[ math.random( 1, #Props ) ] )
@@ -149,7 +40,7 @@ end )
 
 -- Randomly pick a group of props
 hook.Add('PreRoundStart', 'IncomingPropsChange', function()
-    local category = table.Random( table.GetKeys( DefaultProps ) )
+    local category = table.Random( table.GetKeys( GAMEMODE.DefaultProps ) )
     CurrentPropsCategory = category
     
     for k,v in pairs(player.GetAll()) do
@@ -157,21 +48,12 @@ hook.Add('PreRoundStart', 'IncomingPropsChange', function()
     end
 end )
 
--- Get the starting distance to the goal
--- Used for calculating % later
-hook.Add('PlayerSpawn', 'IncomingDistanceSpawn', function(ply)
-    local winent = GAMEMODE:GetWinningEntity()
-    timer.Simple(0.1, function()
-        ply.StartDistance = ply:GetPos():DistToSqr(winent:GetPos())
-    end)
-end)
-
 function GM:GetDistanceToEnd(ply)
-    if not ply.StartDistance then return end
-    
-    local winent = GAMEMODE:GetWinningEntity()
-    local distance = ply:GetPos():DistToSqr(winent:GetPos())
-    local percent = 1 - (distance / ply.StartDistance)
+    local endpos = GAMEMODE:EndingPoint()
+    local distance = ply:GetPos():Distance(endpos)
+    local maxdist = GAMEMODE.MapInfo[game.GetMap()].distance
+    local percent = 1 - (distance / maxdist)
+    if percent < 0 then return end
     
     if ply.BestDistance then
         if percent > ply.BestDistance then
