6662333167bbdd6b853f68da28d76e6beb57b9ef
FluffyXVI <fluffyxvi@gmail.com>
2018-12-05 20:26:55 +1000

    Folder structure for Microgames

---
+++ fluffy_chaos/gamemode/shared.lua
@@ -0,0 +1,17 @@
+DeriveGamemode('fluffy_mg_base')
+
+GM.Name = 'Microgames - Chaos'
+GM.Author = 'FluffyXVI'
+GM.HelpText = [[
+    description pending
+]]
+
+GM.TeamBased = false	-- Is the gamemode FFA or Teams?
+GM.RoundTime = 30      -- Seconds each round lasts for
+GM.RoundType = 'timed'
+GM.GameTime  = 900
+GM.HUDStyle  = 3
+
+function GM:Initialize()
+
+end
