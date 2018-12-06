6e84163b3b3d20ad6bb77abbe9c95f9d518c80a2
FluffyXVI <fluffyxvi@gmail.com>
2018-12-06 20:53:07 +1000

    Fix the 313 issue

--- fluffy_mg_base/gamemode/init.lua
+++ fluffy_mg_base/gamemode/init.lua
@@ -310,6 +310,7 @@ end
 hook.Add('EntityTakeDamage', 'ShotgunGlobalBuff', function(target, dmg)
     local wep = dmg:GetInflictor()
     if wep:GetClass() == 'player' then wep = wep:GetActiveWeapon() end
+    if not IsValid(wep) then return end
     if wep:GetClass() == "weapon_shotgun" then
         dmg:ScaleDamage(2)
     end
