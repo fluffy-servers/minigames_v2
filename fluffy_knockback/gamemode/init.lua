d476053a57ccbd7699613c68184f4dd629e560ec
FluffyXVI <fluffyxvi@gmail.com>
2018-12-09 15:41:00 +1000

    Knockback damage scaling

--- fluffy_knockback/gamemode/init.lua
+++ fluffy_knockback/gamemode/init.lua
@@ -8,10 +8,27 @@ function GM:PlayerLoadout( ply )
     ply:StripWeapons()
     ply:StripAmmo()
     ply:Give('weapon_crowbar')
+    
+    -- Health goes the opposite way to normal in this gamemode
+    -- 500HP = WHOA
+    -- 1HP = fine!
+    ply:SetHealth(1)
+    ply:SetMaxHealth(500)
 end
 
-function GM:EntityTakeDamage(target, dmginfo)
-    if target:IsPlayer() then
-        -- todo
+function GM:EntityTakeDamage(target, dmg)
+    if target:IsPlayer() and dmg:GetAttacker():IsPlayer() then
+        local hp = target:Health()
+        local newdamage = math.random(10, 30) -- random damage
+        target:SetHealth(math.Clamp(hp + newdamage, 0, 500))
+        
+        -- Apply force
+        dmg:SetDamage(0)
+        local f = dmg:GetDamageForce():GetNormalized()
+        f.z = math.abs(f.z)
+        f = f * (hp+newdamage+20)*10
+        f.z = math.Clamp(f.z, 250, 700)
+        target:SetVelocity(f) -- force based on health
+        return true
     end
 end
