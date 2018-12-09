ef7d823d091292461374bab65c417e87a82aca43
FluffyXVI <fluffyxvi@gmail.com>
2018-12-09 16:39:26 +1000

    Folder structures for 2 prototype team gamemodes

---
+++ fluffy_shadows/gamemode/shared.lua
@@ -0,0 +1,12 @@
+DeriveGamemode('fluffy_mg_base')
+
+GM.Name = 'Shadows'
+GM.Author = 'FluffyXVI'
+GM.HelpText = [[
+    to do
+]]
+
+GM.TeamBased = true		-- Is the gamemode FFA or Teams?
+GM.Elimination = false	-- Is this gamemode elimination?
+GM.RoundTime = 120 		-- How long each round should last, in seconds
+GM.RoundNumber = 10		-- How many rounds are in each game?
