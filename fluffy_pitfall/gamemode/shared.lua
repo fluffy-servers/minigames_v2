80c6b61f426bfae33fd0257f1b54412b004ad2f8
FluffyXVI <fluffyxvi@gmail.com>
2018-12-20 21:31:32 +1000

    Rename Tiles back to Pitfall

--- fluffy_pitfall/gamemode/shared.lua
+++ fluffy_pitfall/gamemode/shared.lua
@@ -1,9 +1,17 @@
 DeriveGamemode('fluffy_mg_base')
 
-GM.Name = 'Trembling Tiles'
+GM.Name = 'Pitfall'
 GM.Author = 'FluffyXVI'
 GM.HelpText = [[
-    description pending
+    Try not to fall to your demise!
+    Knock out the platforms other players are standing on.
+    
+    Be the last player alive to win the round!
+    
+    Primary fire will damage platforms.
+    Shoot a platform repeatedly and it will collapse suddenly.
+    
+    Secondary fire will send players flying.
 ]]
 
 GM.TeamBased = false	-- Is the gamemode FFA or Teams?
