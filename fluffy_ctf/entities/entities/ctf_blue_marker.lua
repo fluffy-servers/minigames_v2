0f2ca8e89e30e4d16126a2d75eaba8c48e70256d
FluffyXVI <fluffyxvi@gmail.com>
2018-12-20 18:57:54 +1000

    Fix some seriously stupid wording with flag markers

--- fluffy_ctf/entities/entities/ctf_blue_marker.lua
+++ fluffy_ctf/entities/entities/ctf_blue_marker.lua
@@ -19,11 +19,7 @@ function ENT:Draw()
     surface.SetMaterial(self.Icon)
     surface.DrawTexturedRect(p.x - 8, p.y - 8, 16, 16)
     
-    local text = ''
-    if LocalPlayer():Team() == TEAM_RED then
-        text = 'Attack!'
-    elseif LocalPlayer():Team() == TEAM_BLUE then
-        text = 'Defend!'
+    if LocalPlayer():Team() == TEAM_BLUE then
+        draw.SimpleTextOutlined('Capture here!', 'DermaDefault', p.x, p.y + 16, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
     end
-    draw.SimpleTextOutlined(text, 'DermaDefault', p.x, p.y + 16, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))  
 end
--- fluffy_ctf/entities/entities/ctf_red_marker.lua
+++ fluffy_ctf/entities/entities/ctf_red_marker.lua
@@ -18,11 +18,7 @@ function ENT:Draw()
     surface.SetMaterial(self.Icon)
     surface.DrawTexturedRect(p.x - 8, p.y - 8, 16, 16)
     
-    local text = ''
     if LocalPlayer():Team() == TEAM_RED then
-        text = 'Defend!'
-    elseif LocalPlayer():Team() == TEAM_BLUE then
-        text = 'Attack!'
+        draw.SimpleTextOutlined('Capture here!', 'DermaDefault', p.x, p.y + 16, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
     end
-    draw.SimpleTextOutlined(text, 'DermaDefault', p.x, p.y + 16, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0)) 
 end
