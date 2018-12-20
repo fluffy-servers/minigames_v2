74244b70e3cbf37b5096c1880f0b3fb63299a858
FluffyXVI <fluffyxvi@gmail.com>
2018-12-20 18:56:02 +1000

    Map-defined flag markers added!
    These entities render a flag icon and some basic subtext; to clarify to players where they are supposed to bring the flag

---
+++ fluffy_ctf/entities/entities/ctf_blue_marker.lua
@@ -0,0 +1,29 @@
+AddCSLuaFile()
+ENT.Base = "base_point"
+ENT.Type = "point"
+if CLIENT then
+    ENT.Icon = Material('icon16/flag_blue.png')
+end
+
+function ENT:UpdateTransmitState()
+    return TRANSMIT_ALWAYS
+end
+
+function ENT:Draw()
+    if not LocalPlayer():Alive() or LocalPlayer().Spectating then return end
+    
+    local center = self:GetPos()
+    local p = center:ToScreen()
+    
+    surface.SetDrawColor(color_white)
+    surface.SetMaterial(self.Icon)
+    surface.DrawTexturedRect(p.x - 8, p.y - 8, 16, 16)
+    
+    local text = ''
+    if LocalPlayer():Team() == TEAM_RED then
+        text = 'Attack!'
+    elseif LocalPlayer():Team() == TEAM_BLUE then
+        text = 'Defend!'
+    end
+    draw.SimpleTextOutlined(text, 'DermaDefault', p.x, p.y + 16, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))  
+end
