baad58f529d44eb6c956642de14f1d9d8275a46c
Robert Fraser <fluffyxvi@gmail.com>
2018-12-24 22:12:12 +1000

    Bind shop to F4

--- fluffy_mg_base/gamemode/shop/sv_init.lua
+++ fluffy_mg_base/gamemode/shop/sv_init.lua
@@ -1,3 +1,8 @@
 AddCSLuaFile('cl_inventory.lua')
 AddCSLuaFile('vgui/ShopItemPanel.lua')
 AddCSLuaFile('vgui/ShopMirror.lua')
+
+-- Open the shop on client when F4 is pressed
+function GM:ShowSpare2(ply)
+    ply:ConCommand('minigames_shop')
+end
