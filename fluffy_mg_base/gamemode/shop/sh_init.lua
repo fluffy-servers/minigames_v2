b9c2a22c9d56eb410cdb0a0c856da9e11220907a
Robert Fraser <fluffyxvi@gmail.com>
2018-12-24 22:08:57 +1000

    Shared functions + loading items

--- fluffy_mg_base/gamemode/shop/sh_init.lua
+++ fluffy_mg_base/gamemode/shop/sh_init.lua
@@ -1,4 +1,5 @@
 SHOP = {}
+SHOP.VanillaItems = {}
 
 if SERVER then
     AddCSLuaFile('cl_init.lua')
@@ -6,3 +7,50 @@ if SERVER then
 else
     include('cl_init.lua')
 end
+
+-- Get a hashed version of the table
+-- This should not be used for security, only for verification
+function SHOP:HashTable(tbl)
+    return util.CRC(table.ToString(tbl))
+end
+
+-- Given a key (or information table), grab the rest of the item data
+function SHOP:ParseVanillaItem(key)
+    if type(key) == 'string' then
+        return SHOP.VanillaItems[key]
+    elseif type(key) == 'table' then
+        local k = key.key
+        return table.Merge(SHOP.VanillaItems[k], key)
+    end
+end
+
+-- Registration functions for adding items into the master table
+function SHOP:RegisterHat(ITEM)
+	ITEM.Type = 'Hat'
+	SHOP.VanillaItems[ITEM.VanillaID] = ITEM
+end
+
+function SHOP:RegisterTrail(ITEM)
+	ITEM.Type = 'Trail'
+	SHOP.VanillaItems[ITEM.VanillaID] = ITEM
+end
+
+function SHOP:RegisterTracer(ITEM)
+	ITEM.Type = 'Tracer'
+	SHOP.VanillaItems[ITEM.VanillaID] = ITEM
+end
+
+-- Load all the files
+function SHOP:LoadResources()
+    local path = 'fluffy_mg_base/gamemode/shop/item/'
+    local files, folders = file.Find(path .. '*', 'LUA')
+    for k, v in pairs(folders) do
+        -- Add every item file in subdirectories
+        local files = file.Find(path .. v .. '/*', 'LUA')
+        for k, item in pairs(files) do
+            if SERVER then AddCSLuaFile(path .. v .. '/' .. item, 'LUA' ) end
+            include(path .. v .. '/' .. item)
+        end
+    end
+end
+SHOP:LoadResources()
