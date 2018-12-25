3961c2a7ecb194d12fa6d61d3623b9a1a0229d85
Robert Fraser <fluffyxvi@gmail.com>
2018-12-25 18:37:43 +1000

    Trails are now functioning!

--- fluffy_mg_base/gamemode/shop/sv_equip.lua
+++ fluffy_mg_base/gamemode/shop/sv_equip.lua
@@ -21,3 +21,38 @@ function SHOP:UnequipCosmetic(ITEM, ply)
         net.WriteBool(false)
     net.Broadcast()
 end
+
+-- Equip a trail
+function SHOP:EquipTrail(ITEM, ply)
+    ply.EquippedTrail = ITEM
+    SHOP:WearTrail(ply)
+    return true
+end
+
+-- Unequip a trail
+function SHOP:UnequipTrail(ply)
+    ply.EquippedTrail = nil
+    if ply.TrailEntity then
+        SafeRemoveEntity(ply.TrailEntity)
+    end
+end
+
+-- Wear a trail
+function SHOP:WearTrail(ply)
+    if ply.EquippedTrail then
+        local ITEM = ply.EquippedTrail
+        ply.TrailEntity = util.SpriteTrail(ply, 0, ITEM.Color or color_white, false, 20, 2, 2.5, 0.1, ITEM.Material)
+    end
+end
+
+-- Add equipped trails on player spawn
+hook.Add('PlayerSpawn', 'AddEquippedTrail', function(ply)
+    SHOP:WearTrail(ply)
+end)
+
+-- Remove trails on player death
+hook.Add('DoPlayerDeath', 'RemoveEquippedTrail', function(ply)
+    if ply.TrailEntity then
+        SafeRemoveEntity(ply.TrailEntity)
+    end
+end)
