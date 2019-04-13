14ac0894059353994ecb5071aa7ddb0640e762c6
FluffyXVI <fluffyxvi@gmail.com>
2019-04-13 19:45:17 +1000

    Ball entity code rewrite

--- fluffy_balls/entities/entities/mg_ball_drop.lua
+++ fluffy_balls/entities/entities/mg_ball_drop.lua
@@ -9,10 +9,10 @@ ENT.MaxSize = 64
 ENT.LifeTime = 15
 
 function ENT:SetupDataTables()
-	self:NetworkVar( "Float", 0, "BallSize", { KeyName = "ballsize", Edit = { type = "Float", min = self.MinSize, max = self.MaxSize, order = 1 } } )
-	self:NetworkVar( "Vector", 0, "BallColor", { KeyName = "ballcolor", Edit = { type = "VectorColor", order = 2 } } )
+	self:NetworkVar("Float", 0, "BallSize", { KeyName = "ballsize", Edit = { type = "Float", min = self.MinSize, max = self.MaxSize, order = 1 } })
+	self:NetworkVar("Vector", 0, "BallColor", { KeyName = "ballcolor", Edit = { type = "VectorColor", order = 2 } })
 
-	self:NetworkVarNotify( "BallSize", self.OnBallSizeChanged )
+	self:NetworkVarNotify("BallSize", self.OnBallSizeChanged)
 end
 
 function ENT:Initialize()
@@ -20,14 +20,15 @@ function ENT:Initialize()
 	
 	self:SetModel("models/Combine_Helicopter/helicopter_bomb01.mdl")
 	self:RebuildPhysics()
+    self:SetTrigger(true)
 	
     -- Should be overriden in the gamemode
-	self:SetBallColor( table.Random( {
-		Vector( 1, 0.3, 0.3 ),
-		Vector( 0.3, 1, 0.3 ),
-		Vector( 1, 1, 0.3 ),
-		Vector( 0.2, 0.3, 1 ),
-	} ) )
+	self:SetBallColor(table.Random({
+		Vector(1, 0.3, 0.3),
+		Vector(0.3, 1, 0.3),
+		Vector(1, 1, 0.3),
+		Vector(0.2, 0.3, 1),
+	}))
     
     -- Remove balls that have lived for too long
     timer.Simple(self.LifeTime, function()
@@ -37,76 +38,74 @@ function ENT:Initialize()
     end)
 end
 
-function ENT:RebuildPhysics( value )
+function ENT:RebuildPhysics(value)
 	-- This is necessary so that the vphysics.dll will not crash when attaching constraints to the new PhysObj after old one was destroyed
 	-- TODO: Somehow figure out why it happens and/or move this code/fix to the constraint library
 	self.ConstraintSystem = nil
 
-	local size = math.Clamp( value or self:GetBallSize(), self.MinSize, self.MaxSize ) / 2.1
-	self:PhysicsInitSphere( size, "metal_bouncy" )
-	self:SetCollisionBounds( Vector( -size, -size, -size ), Vector( size, size, size ) )
+	local size = math.Clamp(value or self:GetBallSize(), self.MinSize, self.MaxSize) / 2.1
+	self:PhysicsInitSphere(size, "metal_bouncy")
+	self:SetCollisionBounds(Vector(-size, -size, -size), Vector(size, size, size))
 
 	self:PhysWake()
 end
 
-function ENT:OnBallSizeChanged( varname, oldvalue, newvalue )
+function ENT:OnBallSizeChanged(varname, oldvalue, newvalue)
 	-- Do not rebuild if the size wasn't changed
-	if ( oldvalue == newvalue ) then return end
+	if oldvalue == newvalue then return end
 
-	self:RebuildPhysics( newvalue )
+	self:RebuildPhysics(newvalue)
 end
 
-local BounceSound = Sound( "garrysmod/balloon_pop_cute.wav" )
+local BounceSound = Sound("garrysmod/balloon_pop_cute.wav")
+local BeepSound = Sound("buttons/blip1.wav")
 
-function ENT:PhysicsCollide( data, physobj )
+function ENT:PhysicsCollide(data, physobj)
 	-- Play sound on bounce
-	if ( data.Speed > 60 && data.DeltaTime > 0.2 ) then
-
-		local pitch = 32 + 128 - math.Clamp( self:GetBallSize(), self.MinSize, self.MaxSize )
-		sound.Play( BounceSound, self:GetPos(), 75, math.random( pitch - 10, pitch + 10 ), math.Clamp( data.Speed / 150, 0, 1 ) )
-
+	if data.Speed > 60 and data.DeltaTime > 0.2 then
+		local pitch = 32 + 128 - math.Clamp(self:GetBallSize(), self.MinSize, self.MaxSize)
+		sound.Play(BounceSound, self:GetPos(), 75, math.random(pitch - 10, pitch + 10), math.Clamp(data.Speed / 150, 0, 1))
 	end
 
 	-- Bounce like a crazy bitch
-	local LastSpeed = math.max( data.OurOldVelocity:Length(), data.Speed )
+	local LastSpeed = math.max(data.OurOldVelocity:Length(), data.Speed)
 	local NewVelocity = physobj:GetVelocity()
 	NewVelocity:Normalize()
 
-	LastSpeed = math.max( NewVelocity:Length(), LastSpeed )
+	LastSpeed = math.max(NewVelocity:Length(), LastSpeed)
 
 	local TargetVelocity = NewVelocity * LastSpeed * 0.9
 
-	physobj:SetVelocity( TargetVelocity )
+	physobj:SetVelocity(TargetVelocity)
 end
 
-function ENT:OnTakeDamage( dmginfo )
+function ENT:OnTakeDamage(dmginfo)
 	-- React physically when shot/getting blown
-	self:TakePhysicsDamage( dmginfo )
+	self:TakePhysicsDamage(dmginfo)
 end
 
-function ENT:Use( activator, caller )
-	self:Remove()
-
-	if activator:IsPlayer() then
-		GAMEMODE:CollectBall(activator)
+function ENT:StartTouch(entity)
+    if entity:IsPlayer() then
+        self:Remove()
+		GAMEMODE:CollectBall(entity)
 	end
 end
 
-if ( SERVER ) then return end -- We do NOT want to execute anything below in this FILE on SERVER
+if SERVER then return end -- We do NOT want to execute anything below in this FILE on SERVER
 
-local matBall = Material( "sprites/sent_ball" )
+local matBall = Material("sprites/sent_ball")
 
 function ENT:Draw()
-	render.SetMaterial( matBall )
+	render.SetMaterial(matBall)
 
 	local pos = self:GetPos()
-	local lcolor = render.ComputeLighting( pos, Vector( 0, 0, 1 ) )
+	local lcolor = render.ComputeLighting(pos, Vector(0, 0, 1))
 	local c = self:GetBallColor()
 
-	lcolor.x = c.r * ( math.Clamp( lcolor.x, 0, 1 ) + 0.5 ) * 255
-	lcolor.y = c.g * ( math.Clamp( lcolor.y, 0, 1 ) + 0.5 ) * 255
-	lcolor.z = c.b * ( math.Clamp( lcolor.z, 0, 1 ) + 0.5 ) * 255
+	lcolor.x = c.r * (math.Clamp(lcolor.x, 0, 1) + 0.5) * 255
+	lcolor.y = c.g * (math.Clamp(lcolor.y, 0, 1) + 0.5) * 255
+	lcolor.z = c.b * (math.Clamp(lcolor.z, 0, 1) + 0.5) * 255
 
-	local size = math.Clamp( self:GetBallSize(), self.MinSize, self.MaxSize )
-	render.DrawSprite( pos, size, size, Color( lcolor.x, lcolor.y, lcolor.z, 255 ) )
+	local size = math.Clamp(self:GetBallSize(), self.MinSize, self.MaxSize)
+	render.DrawSprite(pos, size, size, Color(lcolor.x, lcolor.y, lcolor.z, 255))
 end
