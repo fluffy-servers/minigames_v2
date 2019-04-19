00bf016888db66289e8e251a010aa6849349a4e4
FluffyXVI <fluffyxvi@gmail.com>
2019-04-19 18:29:41 +1000

    Climb: Mild rendering updates

--- fluffy_climb/gamemode/cl_init.lua
+++ fluffy_climb/gamemode/cl_init.lua
@@ -1,12 +1,15 @@
 include('shared.lua')
 
 function GM:PostDrawTranslucentRenderables()
+    local goal = GetGlobalInt('ClimbHeight', 0)
+    if goal > 1000 then
+        render.SetColorMaterial()
+        render.DrawQuadEasy(Vector(0, 0, goal), Vector(0, 0, 1), 4096, 4096, Color(0, 255, 0, 150), 0)
+        render.DrawQuadEasy(Vector(0, 0, goal), Vector(0, 0, -1), 4096, 4096, Color(0, 255, 0, 150), 0)
+    end
+    
+    if GetGlobalString('RoundState') != 'InRound' then return end
     render.SetColorMaterial()
     render.DrawQuadEasy(Vector(0, 0, GAMEMODE:GetLavaHeight()), Vector(0, 0, 1), 4096, 4096, Color(255, 0, 0, 150), 0)
     render.DrawQuadEasy(Vector(0, 0, GAMEMODE:GetLavaHeight()), Vector(0, 0, -1), 4096, 4096, Color(255, 0, 0, 150), 0)
-    
-    local goal = GetGlobalInt('ClimbHeight', 0)
-    render.SetColorMaterial()
-    render.DrawQuadEasy(Vector(0, 0, goal), Vector(0, 0, 1), 4096, 4096, Color(0, 255, 0, 150), 0)
-    render.DrawQuadEasy(Vector(0, 0, goal), Vector(0, 0, -1), 4096, 4096, Color(0, 255, 0, 150), 0)
 end
