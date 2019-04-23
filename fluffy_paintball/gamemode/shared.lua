1dd571206051df00aa64194419e85a4e6d52548b
FluffyXVI <fluffyxvi@gmail.com>
2019-04-23 23:02:52 +1000

    Paintball: Update help text

--- fluffy_paintball/gamemode/shared.lua
+++ fluffy_paintball/gamemode/shared.lua
@@ -3,7 +3,13 @@ DeriveGamemode('fluffy_mg_base')
 GM.Name = 'Paintball'
 GM.Author = 'FluffyXVI'
 GM.HelpText = [[
-    to do
+    Eliminate the other team or get the most kills!
+    This is a fast-paced team deathmatch.
+    
+    Collect the weapons scattered around the map!
+    
+    When you are knocked out, you will have to rush back to spawn.
+    If you are knocked out too many times, you will be eliminated.
 ]]
 
 GM.TeamBased = true	-- Is the gamemode FFA or Teams?
