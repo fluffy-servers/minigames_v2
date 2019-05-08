ff90217545d9be65511b0d8737c9adceccf25bab
FluffyXVI <fluffyxvi@gmail.com>
2019-05-09 08:43:53 +1000

    Revert "Infection: Attempt new 'Chase' pathing routine"
    This reverts commit 87e6bcb90b04a5e89c312ece4035873f4a89dc5a.

--- fluffy_infection/entities/entities/npc_zo_base.lua
+++ fluffy_infection/entities/entities/npc_zo_base.lua
@@ -84,7 +84,7 @@ function ENT:Initialize()
     
     if CLIENT then return end
     self.loco:SetDesiredSpeed(self.Speed)
-    self.loco:SetAcceleration(self.Acceleration * 2)
+    self.loco:SetAcceleration(self.Acceleration)
     self.loco:SetDeceleration(self.Acceleration * 8)
     self:SetMaxHealth(self.BaseHealth) -- idk why this isn't clientside but hey, not my fault
 end
@@ -151,8 +151,6 @@ function ENT:FindEnemy()
     local players = team.GetPlayers(TEAM_BLUE)
     local distances = {}
     
-    --if self:HaveEnemy() and self:DistSqrToEnemy() < 500000 then return true end
-    
     -- Get how far away every living player is
     for k,v in pairs(players) do
         if v.Spectating then continue end
@@ -209,7 +207,6 @@ end
 
 -- hi my name is zombie i'm lazy
 function ENT:Idle()
-    self.loco:SetDesiredSpeed(self.Speed)
     self:MovementFunctions(1, self.WalkAnim, 0, 1)
     self:MoveToPos(self:GetPos() + Vector(math.random(-512, 512), math.random(-512, 512), 0), {repath=3, maxage=5})
 end
@@ -218,11 +215,10 @@ end
 function ENT:ChaseEnemy()
     local enemy = self:GetEnemy()
     local pos = enemy:GetPos()
-    self.loco:SetDesiredSpeed(self.Speed)
     self:MovementFunctions(1, self.WalkAnim, 0, 1)
     
     -- Pathing stuff?
-    local path = Path('Chase')
+    local path = Path('Follow')
     path:SetMinLookAheadDistance(300)
     path:SetGoalTolerance(20)
     local result = path:Compute(self, pos)
@@ -232,7 +228,7 @@ function ENT:ChaseEnemy()
         if path:GetAge() > 1 then
             path:Compute(self, self:GetEnemy():GetPos())
         end
-        path:Chase(self, self:GetEnemy())
+        path:Update(self)
         --path:Draw()
         
         -- Ensure we are not stuck
@@ -251,7 +247,7 @@ function ENT:ChaseEnemy()
         end
         
         -- Check the enemy every so often
-        if math.random() > 0.95 then
+        if math.random() > 0.5 then
             self:FindEnemy()
         end
         
