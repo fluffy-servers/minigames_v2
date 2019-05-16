d0a8b68e9e1a77e2f53bac7fcf59e1968df18301
FluffyXVI <fluffyxvi@gmail.com>
2019-05-16 21:42:56 +1000

    Stalker: Fix team names

--- fluffy_stalker/gamemode/shared.lua
+++ fluffy_stalker/gamemode/shared.lua
@@ -16,3 +16,16 @@ GM.RoundNumber = 10
 function GM:Initialize()
 
 end
+
+function GM:CreateTeams()
+	if not GAMEMODE.TeamBased then return end
+	
+	team.SetUp(TEAM_RED, "Stalker", Color( 255, 80, 80 ), true)
+	team.SetSpawnPoint(TEAM_RED, {"info_player_counterterrorist", "info_player_rebel"})
+	
+	team.SetUp(TEAM_BLUE, "Survivors", Color( 80, 80, 255 ), true)
+	team.SetSpawnPoint(TEAM_BLUE, {"info_player_terrorist", "info_player_combine"})
+	
+	team.SetUp(TEAM_SPECTATOR, "Spectators", Color( 255, 255, 80 ), true)
+	team.SetSpawnPoint(TEAM_SPECTATOR, { "info_player_start", "info_player_terrorist", "info_player_combine" }) 
+end
