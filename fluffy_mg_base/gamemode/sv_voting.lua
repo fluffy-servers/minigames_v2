88852c3405cbfe4617b2d1a9f86700e8251ab441
FluffyXVI <fluffyxvi@gmail.com>
2019-08-09 23:00:38 +1000

    Update map rotation

--- fluffy_mg_base/gamemode/sv_voting.lua
+++ fluffy_mg_base/gamemode/sv_voting.lua
@@ -22,7 +22,7 @@ GM.VoteGamemodes = {
     {'fluffy_microgames', 'Microgames', 'FFA'},
     {'fluffy_paintball', 'Paintball', 'Team DM'},
     {'fluffy_climb', 'Climb!', 'FFA'},
-    {'fluffy_ctf', 'Capture The Flag', 'Team DM'},
+    --{'fluffy_ctf', 'Capture The Flag', 'Team DM'},
 }
 
 -- List of maps in rotation
@@ -33,13 +33,13 @@ GM.VoteMaps = {
     fluffy_sniperwars = {'sw_towers', 'sw_stairs_v3', 'sw_toxic_v1', 'sw_iceberg_small', 'sw_grassy'},
     fluffy_poltergeist = {'pg_bigtower', 'pg_stairs'},
     fluffy_duckhunt = {'dh_gauntlet_v2', 'dh_aroundtheblock_v2', 'dh_runforyourlife_v2', 'dh_giants_v1', 'dh_crumble'},
-    fluffy_suicidebarrels = {'sb_snowfall', 'sb_yellobox', 'sb_killingrooms', 'sb_storage_v3', 'sb_pyramid'},
+    fluffy_suicidebarrels = {'sb_snowfall', 'sb_yellobox', 'sb_killingrooms', 'sb_storage_v3', 'sb_pyramid_b2'},
     fluffy_dodgeball = {'db_arena_v3', 'db_terminus_v4', 'db_bunkerchunker', 'db_parkalyptic'},
     fluffy_pitfall = {'pf_ocean'},
     fluffy_incoming = {'inc_duo', 'inc_linear', 'inc_rectangular'},
     fluffy_bombtag = {'bt_diamond', 'bt_rainbow', 'bt_plaza_v2', 'bt_museum', 'bt_canal', 'bt_yeoldearena', 'bt_laserama'},
     fluffy_laserdance = {'ld_toxic', 'ld_rainbow', 'ld_test', 'ld_discus_fix'},
-	fluffy_cratewars = {'cw_bricks', 'cw_boxingring', 'cw_spaceconflict'},
+	fluffy_cratewars = {'cw_bricks', 'cw_boxingring'},
 	fluffy_balls = pvp_maps,
 	fluffy_oitc = pvp_maps_team,
     fluffy_infection = pvp_maps,
