c3c9725832f9593abf9b5fcc3996d011726d3e75
FluffyXVI <fluffyxvi@gmail.com>
2019-08-27 19:41:31 +1000

    Add new trails to the shop

---
+++ fluffy_mg_base/gamemode/shop/item/trail/valve.lua
@@ -0,0 +1,31 @@
+ITEM = {}
+ITEM.VanillaID = 'ammotrail'
+ITEM.Name = 'Spent Shells'
+ITEM.Material = "trails/ammo.vmt"
+ITEM.Rarity = 3
+
+SHOP:RegisterTrail( ITEM )
+
+ITEM = {}
+ITEM.VanillaID = 'hl2trail'
+ITEM.Name = 'Freedom Fighter'
+ITEM.Material = "trails/hl2trail.vmt"
+ITEM.Rarity = 3
+
+SHOP:RegisterTrail( ITEM )
+
+ITEM = {}
+ITEM.VanillaID = 'lambadatrail'
+ITEM.Name = 'Little Lambadas'
+ITEM.Material = "trails/littlelambadas.vmt"
+ITEM.Rarity = 3
+
+SHOP:RegisterTrail( ITEM )
+
+ITEM = {}
+ITEM.VanillaID = 'portaltrail'
+ITEM.Name = 'Portal'
+ITEM.Material = "trails/portal.vmt"
+ITEM.Rarity = 3
+
+SHOP:RegisterTrail( ITEM )
