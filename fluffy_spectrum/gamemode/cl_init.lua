15ce17f147dbbb130cb784ab7c998b0a53f51a5e
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-02-23 12:19:13 +1000

    Spectrum: Prototype paint splatter decals

--- fluffy_spectrum/gamemode/cl_init.lua
+++ fluffy_spectrum/gamemode/cl_init.lua
@@ -1,1 +1,19 @@
 include('shared.lua')
+
+GAMEMODE.PaintSplat = Material('decals/decal_paintsplatterpink001')
+
+-- Paintball effects for every bullet
+hook.Add('EntityFireBullets', 'PaintSplatterEffects', function(ent, data)
+    if ent:IsWeapon() then
+        ent = ent:GetOwner()
+    end
+    data.Callback = function(attacker, tr, dmg)
+	    if tr.HitSky then return end
+    
+        local c = team.GetColor(ent:Team())
+        local s = 0.25 + 0.3*math.random()
+        util.DecalEx(GAMEMODE.PaintSplat, tr.HitEntity or game.GetWorld(), tr.HitPos, tr.HitNormal, c, s, s)
+    end
+
+    return true
+end)
