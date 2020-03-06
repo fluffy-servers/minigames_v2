aca7ee6d397fd2120e47423772d30825960bde5d
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-03-07 00:00:21 +1000

    Gun Game: FIx scoring pane

--- fluffy_gungame/gamemode/cl_init.lua
+++ fluffy_gungame/gamemode/cl_init.lua
@@ -3,8 +3,7 @@ include('shared.lua')
 surface.CreateFont('GunGameFont', { font='HL2MP', size=32 })
 surface.CreateFont('GunGameFontBig', { font='HL2MP', size=72 })
 
--- The scoring pane is different from that provided in the base gamemode
--- Instead of displaying a raw number, we show an icon of the current weapon instead
+GM.ScoringPaneEnabled = true
 
 -- Timer to update the scoring pane every three seconds
 ScoreRefreshPlayers = timer.Create('RefreshPlayers', 3, 0, function()
@@ -37,17 +36,21 @@ ScoreRefreshPlayers = timer.Create('RefreshPlayers', 3, 0, function()
     end
 end)
 
-function CreateScoringPane()
+-- The scoring pane is different from that provided in the base gamemode
+-- Instead of displaying a raw number, we show an icon of the current weapon instead
+function GM:CreateScoringPane()
     if GAMEMODE.ScorePane then
         GAMEMODE.ScorePane:Remove()
+        print('Removing old pane')
     end
 
     local Frame = vgui.Create('DPanel')
-    Frame:SetSize(ScrW() * 0.5, 96)
-    Frame:SetPos(ScrW() * 0.25, ScrH() - 72)
+    local panel_h = 80
+    Frame:SetSize(ScrW() * 0.5, panel_h)
+    Frame:SetPos(ScrW() * 0.25, ScrH() - panel_h)
     
     function Frame:CreatePlayer(ply, x)
-        local p = vgui.Create('DPanel', Frame)
+        local p = vgui.Create('DPanel', self)
         p:SetPos(x, 0)
         p:SetSize(64, 64)
         function p:Paint()
@@ -63,18 +66,39 @@ function CreateScoringPane()
         Avatar:SetPos(14, 0)
         Avatar:SetPlayer(ply, 64)
     end
-    
+
     function Frame:Paint()
-    
+
     end
+
     GAMEMODE.ScorePane = Frame
 end
 
 -- Render the sidebar on the left
 -- This displays the weapons and the current progress of the player
 hook.Add('HUDPaint', 'GungameCoolHUD', function()
-    if !IsValid(ScorePane) then CreateScoringPane() end
+    -- Obey the convar
+    local shouldDraw = GetConVar('cl_drawhud'):GetBool()
+    if not shouldDraw then 
+		if GAMEMODE:ScoringPaneActive() and IsValid(GAMEMODE.ScorePane) then
+			GAMEMODE.ScorePane:Hide()
+		end
+        
+		return 
+	end
+
+	-- Scoring pane creation
+	if GAMEMODE:ScoringPaneActive() then
+		if !IsValid(GAMEMODE.ScorePane) then 
+			GAMEMODE:CreateScoringPane() 
+		else
+			GAMEMODE.ScorePane:Show()
+		end
+	elseif IsValid(GAMEMODE.ScorePane) then
+        GAMEMODE.ScorePane:Hide()
+    end
     
+    -- Draw the weaponry sidebar
     local current = math.floor(LocalPlayer():GetNWInt('GG_Progress', 0) / 2) + 1
     local icons = GAMEMODE.WeaponIcons
 
@@ -90,6 +114,7 @@ end)
 -- Add halos for any players on the last stage
 -- This table is calculated in the the ScoreRefreshPlayers
 hook.Add('PreDrawHalos', 'DrawKingHalo', function()
+    if true then return end
     if not GAMEMODE.DangerPlayers then return end
 
     for _,v in pairs(GAMEMODE.DangerPlayers) do
