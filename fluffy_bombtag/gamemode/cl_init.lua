38d95bdfdbcb8fd579a9279e8a18699d00343a92
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-03-06 23:15:26 +1000

    Bomb Tag: Fix bug with halos

--- fluffy_bombtag/gamemode/cl_init.lua
+++ fluffy_bombtag/gamemode/cl_init.lua
@@ -3,7 +3,7 @@ include('shared.lua')
 -- Draw a halo around whoever has the bomb
 hook.Add('PreDrawHalos', 'DrawBombHalo', function()
     for k,v in pairs(player.GetAll()) do
-        if v:IsCarrier() then
+        if v:GetNWBool("Carrier", false) then
             local pcolor = v:GetPlayerColor()
             local color = Color(pcolor[1]*255, pcolor[2]*255, pcolor[3]*255)
             halo.Add({v}, color, 2, 2, 2, true, true)
