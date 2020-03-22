7f846012751e0d22a0d58d08cb8f441a37601af7
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-03-22 22:28:25 +1000

    Microgames: Antlion survival modifier
    Thankyou Xobile for this modifier!
    
    Co-Authored-By: Xobile <elibox@users.noreply.github.com>

---
+++ fluffy_microgames/gamemode/modifiers/antlionguard.lua
@@ -0,0 +1,30 @@
+MOD.Name = 'Antlionguard 2: Electric Boogaloo'
+MOD.Region = 'empty'
+
+local function spawnAntlionGuard()
+    local number = GAMEMODE:PlayerScale(0.75, 4, 10)
+    local positions = GAMEMODE:GetRandomLocations(number, 'ground')
+
+    for i=1,number do
+        local pos = positions[i]
+        local ent = ents.Create("npc_antlionguard") 
+        ent:SetPos(pos)
+        ent:Spawn()
+
+    end
+end
+
+function MOD:Initialize()
+    spawnAntlionGuard()
+    GAMEMODE:Announce("Anybody got bugbait?")
+end
+
+function MOD:Loadout(ply)
+    ply:SetMaxHealth(1)
+    ply:SetHealth(1)
+    ply:SetRunSpeed(225)
+end
+    
+function MOD:EntityTakeDamage(ent, dmg)
+    if not ent:IsPlayer() then return end
+end
