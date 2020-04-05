450501bdec186b1fe458e638cc05003466fb4bc4
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-04-05 17:55:15 +1000

    Update weapon spawner effect

--- fluffy_mg_base/entities/effects/spawner_ready.lua
+++ fluffy_mg_base/entities/effects/spawner_ready.lua
@@ -2,36 +2,45 @@ EFFECT.Mat = Material('sprites/physg_glow1')
 --sprites/light_ignorez
 --sprites/physg_glow1
 --sprites/glow04_noz
+--sprites/sent_ball
+--sprites/key_0
+--sprites/key_10
+--sprites/hud1
+--sprites/tp_beam001
+--sprites/dot
+--sprites/animglow02
 
 function EFFECT:Init(data)
-    self.Size = 24
-    self.Alpha = 255
+    self.Radius = 24
+    self.Life = 1
     self.Number = 5
     self.Pos = data:GetOrigin()
+
     self.Angle = 0
     self.AngOff = (math.pi / self.Number) * 2
-    self.Z = -16
-
-    self.Color = HSVToColor(CurTime()*100 % 360, 1, 1)
+    self.Z = 0
+    self.ParticleSize = 12
+    self.Duration = 5
 end
 
 function EFFECT:Think()
-    self.Alpha = self.Alpha - FrameTime()*75
-    self.Angle = self.Angle + FrameTime()*2
-    self.Z = self.Z + FrameTime()*15
-    return (self.Alpha > 0)
+    self.Life = self.Life - (FrameTime()/self.Duration)
+    self.Angle = self.Angle + FrameTime()*1
+    self.Z = self.Z + FrameTime()*20
+
+    return (self.Life > 0)
 end
 
 function EFFECT:Render()
     local p = self.Pos
-
-    local c = self.Color
-    c.a = 255
+    local c = HSVToColor(CurTime()*20 % 360, 1, 1)
+    c.a = 255 * 4 * (self.Life - (self.Life * self.Life))
 
     for i=1,self.Number do
         local angle = self.Angle + i*self.AngOff
-        local p = self.Pos + Vector(self.Size * math.cos(angle), self.Size * math.sin(angle), self.Z + i)
+        local p = self.Pos + Vector(self.Radius * math.cos(angle), self.Radius * math.sin(angle), self.Z)
+        local ps = self.ParticleSize
         render.SetMaterial(self.Mat)
-        render.DrawSprite(p, 12, 12, c)
+        render.DrawSprite(p, ps, ps, c)
     end
 end
