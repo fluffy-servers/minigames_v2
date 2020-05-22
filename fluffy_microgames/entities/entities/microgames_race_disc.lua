2a0c81a2afe79af8d76366b60e2bff4cc77a261a
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-05-23 05:23:50 +1000

    Microgames: Race modifier first build

---
+++ fluffy_microgames/entities/entities/microgames_race_disc.lua
@@ -0,0 +1,39 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "microgames_disc"
+ENT.Radius = 32
+ENT.Icon = Material('icon16/flag_green.png', 'noclamp')
+
+if CLIENT then
+    ENT.Circle = Material("sprites/sent_ball")
+
+    function ENT:Draw()
+        local radius = self:GetNWInt("Radius", self.Radius)
+        local point = self:GetNWInt("RacePoint", 0)
+        render.SetMaterial(self.Circle)
+        render.DrawQuadEasy(self:GetPos() + Vector(0, 0, 1), self:GetUp(), radius*2, radius*2, self:GetColor())
+
+        -- Stick a big number on the circle
+        cam.Start3D2D(self:GetPos() + Vector(0, 0, 1), Angle(0, 0, 00), 1/4)
+            draw.SimpleTextOutlined(point, 'FS_128', 0, 0, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0, 0, 0))
+        cam.End3D2D()
+
+        if not LocalPlayer():Alive() or LocalPlayer().Spectating then return end
+        local center = self:GetPos() + Vector(0, 0, 32)
+        local p = center:ToScreen()
+        local size = 24
+
+        if (point - 1) == LocalPlayer():GetNWInt("RaceState", 0) then
+            cam.Start2D()
+                -- Mark this race point
+                draw.NoTexture()
+                surface.SetMaterial(self.Icon)
+                surface.SetDrawColor(color_white)
+                surface.DrawTexturedRect(p.x - size/2, p.y - size/2, size, size)
+
+                draw.SimpleTextOutlined('Go here!', 'FS_24', p.x, p.y + size + 4, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0, 0, 0))
+            cam.End2D()
+        end
+    end
+end
---
+++ fluffy_microgames/gamemode/modifiers/race.lua
@@ -0,0 +1,70 @@
+MOD.Name = 'Race'
+MOD.Countdown = true
+
+MOD.SurviveValue = 2
+MOD.RoundTime = 30
+
+local function spawnDisc(pos, color)
+    local ent = ents.Create('microgames_race_disc')
+    ent:SetPos(pos)
+    ent:SetColor(color)
+    ent:Spawn()
+    return ent
+end
+
+function MOD:SpawnCircles()
+    GAMEMODE.NumberNodes = math.random(3, 8)
+
+    local positions = GAMEMODE:GetRandomLocations(GAMEMODE.NumberNodes, 'crate')
+    local colors = table.Shuffle(GAMEMODE.DiscColors)
+    GAMEMODE.RaceNodes = {}
+    for i=1,GAMEMODE.NumberNodes do
+        local circle = spawnDisc(positions[i], colors[i][2])
+        circle:SetNWInt("RacePoint", i)
+        table.insert(GAMEMODE.RaceNodes, circle)
+    end
+end
+
+function MOD:Initialize()
+    self:SpawnCircles()
+    GAMEMODE:Announce("Race", "Go go go!")
+end
+
+function MOD:Loadout(ply)
+    ply:SetNWInt("RaceState", 0)
+    ply:SetRunSpeed(350)
+    ply:SetWalkSpeed(300)
+    ply:Give("weapon_crowbar")
+end
+
+MOD.ThinkTime = 0.250
+function MOD:Think()
+    for number, disc in pairs(GAMEMODE.RaceNodes) do
+        local playersOnDisc = disc:GetPlayers()
+        for k, v in pairs(playersOnDisc) do
+            if v:GetNWInt("RaceState", 0) == (number - 1) then
+                v:SetNWInt("RaceState", number)
+            end
+        end
+    end
+end
+
+function MOD:Cleanup()
+    for k,v in pairs(player.GetAll()) do
+        if not v:Alive() then continue end
+        if v:GetNWInt("RaceState", 0) != GAMEMODE.NumberNodes then
+            v:Kill()
+        end
+    end
+end
+
+function MOD:EntityTakeDamage(ent, dmg)
+    if not ent:IsPlayer() then return end
+    if not dmg:GetAttacker():IsPlayer() then return true end
+    
+    dmg:SetDamage(0)
+    local v = dmg:GetDamageForce():GetNormalized()
+    v.z = math.max(math.abs(v.z) * 0.5, 0.0025)
+    ent:SetGroundEntity(nil)
+    ent:SetVelocity(v * 800)
+end
