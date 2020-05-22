759aef2aac61369bb327f950efe7915db11713c2
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-05-23 03:28:25 +1000

    Microgames: Balloons modifier

---
+++ fluffy_microgames/entities/entities/microgames_balloon.lua
@@ -0,0 +1,106 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+
+-- Balloon properties
+ENT.PrintName = "Balloon"
+ENT.Model = "models/maxofs2d/balloon_classic.mdl"
+ENT.Points = 1
+ENT.SpeedMin = 20
+ENT.SpeedMax = 30
+ENT.Balloon = true
+
+ENT.BalloonTypes = {
+    classic = {
+        points = 1,
+        minspeed = 20,
+        maxspeed = 30,
+        model = "models/maxofs2d/balloon_classic.mdl"
+    },
+
+    heart = {
+        points = 1,
+        minspeed = 20,
+        maxspeed = 30,
+        model = "models/maxofs2d/balloon_classic.mdl"
+    },
+
+    star = {
+        points = 1,
+        minspeed = 20,
+        maxspeed = 30,
+        model = "models/maxofs2d/balloon_classic.mdl"
+    }
+}
+
+-- Create the balloon
+function ENT:Initialize()
+    -- Select the type of balloon
+    local r = math.random()
+    local bType = 'classic'
+    if r < 0.1 then
+        btype = 'star'
+    elseif r < 0.25 then
+        btype = 'heart'
+    end
+
+    -- Load balloon properties
+    local bTable = self.BalloonTypes[btype]
+    self.Speed = math.random(bTable.minspeed, bTable.maxspeed)
+    self.Points = bTable.points
+    self:SetModel(bTable.model)
+    self:SetColor(HSVToColor(math.random(360), 1, 1))
+    self:SetRenderMode(RENDERMODE_TRANSALPHA)
+
+    -- Create physics object
+    self:PhysicsInit(SOLID_VPHYSICS)
+    local phys = self:GetPhysicsObject()
+    if IsValid(phys) then
+        phys:SetMass(100)
+        phys:Wake()
+        phys:EnableGravity(false)
+    end
+    self:StartMotionController()
+
+    -- Add a little bit of sideways velocity
+    self.SideMotion = Vector(math.Rand(-1, 1), math.Rand(-1, 1), 0) * self.Speed
+end
+
+-- Pop the balloon if we hit the world
+function ENT:PhysicsCollide(data, phys)
+    if data.HitEntity == game.GetWorld() then
+        -- Balloon prop effect
+        local c = self:GetColor()
+        local ed = EffectData()
+        ed:SetOrigin(self:GetPos())
+        ed:SetStart(Vector(c.r, c.g, c.b))
+        util.Effect("balloon_pop", ed)
+
+        self:Remove()
+    end
+end
+
+-- Handle balloon popping effects
+function ENT:OnTakeDamage(dmginfo)
+    -- Balloon prop effect
+    local c = self:GetColor()
+    local ed = EffectData()
+    ed:SetOrigin(self:GetPos())
+    ed:SetStart(Vector(c.r, c.g, c.b))
+    util.Effect("balloon_pop", ed)
+
+    -- Register this with the gamemode
+    local attacker = dmginfo:GetAttacker()
+    if IsValid(attacker) and attacker:IsPlayer() then
+        hook.Run("PropBreak", attacker, self)
+    end
+    self:Remove()
+end
+
+-- Make the balloon physically float upwards
+function ENT:PhysicsSimulate(phys, delta)
+    local vLinear = Vector(0, 0, (self.Speed or 5) * 5000 * delta) + (self.SideMotion * 500 * delta)
+    local vAngular = Vector(0, 0, 0)
+
+    return vAngular, vLinear, SIM_GLOBAL_FORCE
+end
---
+++ fluffy_microgames/gamemode/modifiers/balloons.lua
@@ -0,0 +1,39 @@
+MOD.Name = 'Balloons'
+
+MOD.Region = 'knockback'
+MOD.ScoreValue = 0.1
+MOD.ScoringPane = true
+
+MOD.WinValue = 3
+MOD.RoundTime = 25
+
+local function spawnBalloons()
+    local number = GAMEMODE:PlayerScale(0.4, 3, 8)
+    local positions = GAMEMODE:GetRandomLocations(number, 'edge')
+
+    for i=1,number do
+        local pos = positions[i]
+        local ent = ents.Create("microgames_balloon")
+        ent:SetPos(pos + Vector(0, 0, 8))
+        ent:Spawn()
+    end
+end
+
+function MOD:Initialize()
+    spawnBalloons()
+    GAMEMODE:Announce("Balloons!", "Pop as many as you can!")
+end
+
+function MOD:Loadout(ply)
+    ply:Give('weapon_mg_pistol')
+end
+
+function MOD:PropBreak(ply, prop)
+    if prop:GetClass() != "microgames_balloon" then return end
+    ply:AddMScore(prop.Score or 1)
+end
+
+MOD.ThinkTime = 1
+function MOD:Think()
+    spawnBalloons()
+end
