9400d43e91a8857b513914cb7b475ed05cc1cf57
Xobile <knightroonik@gmail.com>
2020-06-30 20:48:59 +1000

    changed adrenaline model to health vial
    holy shit how does this work

--- fluffy_infection/entities/weapons/inf_adrenaline.lua
+++ fluffy_infection/entities/weapons/inf_adrenaline.lua
@@ -1,23 +1,44 @@
-SWEP.Base = 'weapon_mg_base'
+SWEP.Base = 'weapon_mg_sck_base'
+SWEP.HoldType = 'grenade'
 
 if CLIENT then
     SWEP.IconFont = "CSSelectIcons"
     SWEP.IconLetter = "H"
+
+
+    SWEP.PrintName = 'Adrenaline'
+    SWEP.Slot = 2
+    SWEP.SlotPos = 2
 end
 
--- Menu data
-SWEP.PrintName = 'Cloaking Device'
-SWEP.Slot = 2
-SWEP.SlotPos = 2
-
 -- Model data
-SWEP.HoldType = 'slam'
-SWEP.ViewModel	= "models/weapons/cstrike/c_c4.mdl"
-SWEP.WorldModel = "models/weapons/w_c4.mdl"
+SWEP.ViewModel = "models/weapons/c_grenade.mdl"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
 SWEP.UseHands = true
 
+SWEP.WorldModel = "models/weapons/w_grenade.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+
+SWEP.ViewModelBoneMods = {
+    ["ValveBiped.Grenade_body"] = { scale = Vector(0.326, 0.326, 0.326), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
+}
+ 
+SWEP.VElements = {
+    ["dynamite"] = { type = "Model", model = "models/healthvial.mdl", bone = "ValveBiped.Grenade_body", rel = "", pos = Vector(0.657, -0.401, -5.705), angle = Angle(0, 0, 0), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+ 
+SWEP.WElements = {
+    ["dynamite"] = { type = "Model", model = "models/healthvial.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(5.164, 1.86, -4.199), angle = Angle(18.398, 10.642, -4.481), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+
+function SWEP:Initialize()
+    return self.BaseClass.Initialize(self)
+end
+
 -- Effect data
-SWEP.Primary.Sound = Sound("ambient/machines/teleport3.wav")
+SWEP.Primary.Sound = Sound("combined/k_lab/k_lab_kl_charger01_cc.wav")
 SWEP.Primary.Delay = 1.5
 
 -- Reset utility on player spawn
