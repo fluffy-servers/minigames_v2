cdfd883156b99a38020b1125baebe49ab5cddc05
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-07-17 23:24:47 +1000

    Spectrum: Team halos

--- fluffy_spectrum/gamemode/cl_init.lua
+++ fluffy_spectrum/gamemode/cl_init.lua
@@ -1,21 +1,10 @@
 include('shared.lua')
 
-GM.PaintSplat = Material('decals/decal_paintsplatterpink001')
+-- Halos around your team members
+hook.Add('PreDrawHalos', 'DrawSpectrumHalos', function()
+    if not LocalPlayer():Alive() or LocalPlayer().Spectating then return end
+    local players = team.GetPlayers(LocalPlayer():Team())
+    local color = team.GetColor(LocalPlayer():Team())
 
-/*
--- Paintball effects for every bullet
-hook.Add('EntityFireBullets', 'PaintSplatterEffects', function(ent, data)
-    if ent:IsWeapon() then
-        ent = ent:GetOwner()
-    end
-    data.Callback = function(attacker, tr, dmg)
-	    if tr.HitSky then return end
-    
-        local c = team.GetColor(ent:Team())
-        local s = 0.25 + 0.3*math.random()
-        util.DecalEx(GAMEMODE.PaintSplat, tr.HitEntity or game.GetWorld(), tr.HitPos, tr.HitNormal, c, s, s)
-    end
-
-    return true
+    halo.Add(players, color, 2, 2, 2)
 end)
-*/
