b4ecc6ae2b5cc5ec4d79bb2b118fd9e9768581ff
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-09-16 00:01:57 +1000

    Classics gamemode scaffold

---
+++ fluffy_classics/gamemode/init.lua
@@ -0,0 +1,19 @@
+-- Send the required files to clients & include shared
+AddCSLuaFile('cl_init.lua')
+AddCSLuaFile('shared.lua')
+include('shared.lua')
+
+-- No weapons on loadout
+function GM:PlayerLoadout(ply)
+	-- Strip any old weapons & ammo just in case
+	ply:StripWeapons()
+	ply:StripAmmo()
+
+	ply:SetWalkSpeed(200)
+	ply:SetRunSpeed(400)
+end
+
+-- Disable picking up the same weapon twice
+hook.Add("PlayerCanPickupWeapon", "StopDoublePickup", function(ply, wep)
+	if ply:HasWeapon(wep:GetClass()) then return false end
+end)
---
+++ fluffy_classics/gamemode/shared.lua
@@ -0,0 +1,12 @@
+DeriveGamemode('fluffy_mg_base')
+
+GM.Name = 'Classics'
+GM.Author = 'FluffyXVI'
+GM.HelpText = [[
+    Classic Minigames maps, from the CS:S era
+]]
+
+GM.TeamBased = true		-- Is the gamemode FFA or Teams?
+GM.Elimination = true	-- Is this gamemode elimination?
+GM.RoundTime = 120 		-- How long each round should last, in seconds
+GM.RoundNumber = 10		-- How many rounds are in each game?
