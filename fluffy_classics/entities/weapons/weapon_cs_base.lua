98acafb149ea243a56a1e7a9e867099f9812cb77
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-09-16 21:56:40 +1000

    Adjust weapon damage

--- fluffy_classics/entities/weapons/weapon_cs_base.lua
+++ fluffy_classics/entities/weapons/weapon_cs_base.lua
@@ -26,9 +26,9 @@ function SWEP:PrimaryAttack()
     local scaled_damage = math.floor(self.Primary.Damage * 0.5)
     
     self.Weapon:EmitSound(self.Primary.Sound)
-	self:ShootBulletEx(scaled_damage, self.Primary.NumShots, self.Primary.Cone * 20, self.Primary.Tracer)
+	self:ShootBulletEx(scaled_damage, self.Primary.NumShots, self.Primary.Cone * 5, self.Primary.Tracer)
     self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
     self:SetNextSecondaryFire(CurTime() + self.Primary.Delay)
     self:TakePrimaryAmmo(1)
-    self.Owner:ViewPunch(Angle(math.Rand(-0.5, -0.2) * self.Primary.Recoil * 5, math.Rand(-0.1, 0.1) * self.Primary.Recoil * 5, 0))
+    self.Owner:ViewPunch(Angle(math.Rand(-0.4, -0.2) * self.Primary.Recoil * 5, math.Rand(-0.1, 0.1) * self.Primary.Recoil * 5, 0))
 end
--- fluffy_classics/entities/weapons/weapon_knife.lua
+++ fluffy_classics/entities/weapons/weapon_knife.lua
@@ -1,1 +1,3 @@
 SWEP.Base = 'weapon_mg_knife'
+SWEP.Damage = 40
+SWEP.Primary.Damage = 40
