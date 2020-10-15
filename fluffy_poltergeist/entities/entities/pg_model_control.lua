78227609ec8e95b5a35d86a07cbba4665633cd1c
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-10-15 15:05:33 +1000

    Fix all the bugs I added in the previous commits

--- fluffy_poltergeist/entities/entities/pg_model_control.lua
+++ fluffy_poltergeist/entities/entities/pg_model_control.lua
@@ -1,17 +1,12 @@
 ENT.Type = "point"
 
---[[
-function ENT:Initialize()
-    GAMEMODE.BarrelModels = ["models/props_c17/oildrum001_explosive.mdl"]
-    GAMEMODE.BarrelSkins = [0]
-end
-]]
-function ENT:Initialize()
-    GAMEMODE.PropModels = {}
-end
-
 -- KV properties for mapping data
 function ENT:KeyValue(key, value)
+    if not self.Ready then
+        GAMEMODE.PropModels = {}
+        self.Ready = true
+    end
+
     if string.StartWith(key, "model") then
         table.insert(GAMEMODE.PropModels, value)
     end
---
+++ fluffy_poltergeist/entities/entities/pg_spawner.lua
@@ -0,0 +1,39 @@
+﻿ENT.Type = "point"
+ENT.RespawnTime = 3
+
+function ENT:KeyValue(key, value)
+    if key == "frequency" then
+        self.RespawnTime = math.Clamp(tonumber(value), 1, 60)
+    end
+end
+
+function ENT:Think()
+    if (self.Timer or 0) < CurTime() then
+        self.Timer = CurTime() + self.RespawnTime
+        self:SpawnProp()
+    end
+end
+
+function ENT:SpawnProp()
+    if table.Count(ents.FindByClass("prop_phys*")) > GAMEMODE.MaxProps then return end
+    local prop = self:CreateProp(table.Random(GAMEMODE.PropModels))
+    local phys = prop:GetPhysicsObject()
+
+    if phys and phys:IsValid() then
+        phys:AddAngleVelocity(Vector((VectorRand() * 200):Angle()))
+    end
+end
+
+function ENT:CreateProp(model)
+    local prop = ents.Create("prop_physics")
+    prop:SetPos(self:GetPos())
+    prop:SetAngles(self:GetAngles())
+    prop:SetModel(model)
+    prop:SetSkin(math.random(0, prop:SkinCount()-1))
+    prop:Spawn()
+    return prop
+end
+
+function ENT:GetFrequency()
+    return self.RespawnTime
+end
